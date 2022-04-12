f37d7493f4e693724bf5cede632a3a100fea05fa
fernandovalenzuela <fernandovalenzuelaibarra@gmail.com>
2022-04-11 22:37:19 -0400

    first  commit

---
+++ src/GitExpertApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const GitExpertApp = () => {
+  return (
+    <>
+        <h2>GitExpertApp</h2>
+    </>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import reactDom from 'react-dom'
+import { GitExpertApp } from './GitExpertApp';
+
+import './index.css';
+
+reactDom.render(
+    <GitExpertApp/>,
+    document.getElementById('root')
+);
